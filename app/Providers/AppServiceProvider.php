04f6661d77931b7d26d7eb285964cbd506f5aaa1
Diego Goulart <diegogoulart.aws@gmail.com>
2024-03-24 00:06:56 +0000

    feat: Add PixService and CustomerService bindings to AppServiceProvider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,11 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Services\PaymentService;
+use App\Services\CustomerService;
+use App\Services\PixService;
 use Core\Domain\Application\Contracts\PaymentProcessor;
+use Core\Domain\Application\Contracts\PixProcessor;
+use Core\Domain\Application\Contracts\CustomerContract;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +18,8 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(PaymentProcessor::class, PaymentService::class);
+        $this->app->bind(PixProcessor::class, PixService::class);
+        $this->app->bind(CustomerContract::class, CustomerService::class);
     }
 
     /**
