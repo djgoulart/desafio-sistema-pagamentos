2754a7c001f5e64a71e7eeca83bedc08c33a775f
Diego Goulart <diegogoulart.aws@gmail.com>
2024-03-24 00:08:15 +0000

    feat: Refactor CustomerService to use CustomerDto and improve error handling.

--- app/Services/CustomerService.php
+++ app/Services/CustomerService.php
@@ -3,7 +3,8 @@
 namespace App\Services;
 
 use Core\Domain\Enterprise\Entities\Customer;
-use Core\Domain\Application\Contracts\Customer as CustomerContract;
+use Core\Domain\Enterprise\Dtos\CustomerDto;
+use Core\Domain\Application\Contracts\CustomerContract;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Http;
 
@@ -12,6 +13,8 @@ class CustomerService implements CustomerContract
     public function createCustomer(CustomerDto $customer)
     {
         $apiUrl = Config::get('services.asaas.url');
+
+        //$customerData = $customer->getData();
 
         $requestData = [
             'name' => $customer->name,
@@ -24,7 +27,23 @@ class CustomerService implements CustomerContract
             'access_token' => Config::get('services.asaas.token')
         ])->post("$apiUrl/customers", $requestData);
 
-        return $response->object();
+        if($response->successful()) {
+            $respData = $response->json();
+            $customerData = new CustomerDto(
+                externalId: $respData['id'],
+                name: $respData['name'],
+                cpfCnpj: $respData['cpfCnpj'],
+            );
+
+            $customerFromDomain = new Customer(customerData: $customerData);
+            return $customerFromDomain->getData();
+        }
+
+        if($response->failed()) {
+            $respData = $response->json();
+            throw new \Exception($respData['errors'][0]['description']);
+        }
+
 
         // Aqui, você pode adicionar mais lógica como salvar o pagamento no banco de dados,
         // enviar notificações, etc., dependendo dos requisitos da sua aplicação.
